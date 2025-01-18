9d69a29d3ebd7f39272d170e045aa522217bd7f3
mvl-boston <manuel@alumni.virginia.edu>
2025-01-18 09:06:27 -0500

    Update values.py
    updated strongest breaking to match nidec

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -30,7 +30,7 @@ class CarControllerParams:
   NIDEC_GAS_MAX = 198  # 0xc6
   NIDEC_BRAKE_MAX = 1024 // 4
 
-  BOSCH_ACCEL_MIN = -3.5  # m/s^2
+  BOSCH_ACCEL_MIN = -4.0  # m/s^2   - try same bigger min as for NIDEC
   BOSCH_ACCEL_MAX = 2.0  # m/s^2
 
   BOSCH_GAS_LOOKUP_BP = [-0.2, 2.0]  # 2m/s^2
