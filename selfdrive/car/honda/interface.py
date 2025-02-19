1c1a89b9b1ff176b5524a50b6ef63a294aa16bff
mvl-boston <manuel@alumni.virginia.edu>
2025-02-18 21:12:52 -0500

    No kiv kpv

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -72,15 +72,9 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.pid.kf = 0.00006  # conservative feed-forward
 
     if candidate in HONDA_BOSCH:
-      # ret.longitudinalTuning.kpV = [0.25]
-      # ret.longitudinalTuning.kiV = [0.05]
-      
-      # use default Honda tuning, revise pedal curve  
-      ret.longitudinalTuning.kpBP = [0., 5., 35.]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5] 
-      ret.longitudinalTuning.kiBP = [0., 35.]
-      ret.longitudinalTuning.kiV = [0.18, 0.12] 
-      
+      ret.longitudinalTuning.kpV = [0.0]
+      ret.longitudinalTuning.kiV = [0.0] # remove since brake and gas have different tune
+  
       ret.longitudinalActuatorDelayUpperBound = 0.5 # s
       if candidate in HONDA_BOSCH_RADARLESS:
         ret.stopAccel = CarControllerParams.BOSCH_ACCEL_MIN  # stock uses -4.0 m/s^2 once stopped but limited by safety model
