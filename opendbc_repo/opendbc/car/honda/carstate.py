bc5f33dbe40c09c7b928e09e197515d6803fa2f2
mvl-boston <manuel@alumni.virginia.edu>
2025-02-22 13:37:15 -0500

    Lowspeed warning message

--- opendbc_repo/opendbc/car/honda/carstate.py
+++ opendbc_repo/opendbc/car/honda/carstate.py
@@ -149,7 +149,10 @@ class CarState(CarStateBase):
     ret.steerFaultPermanent = steer_status not in ("NORMAL", "NO_TORQUE_ALERT_1", "NO_TORQUE_ALERT_2", "LOW_SPEED_LOCKOUT", "TMP_FAULT")
     # LOW_SPEED_LOCKOUT is not worth a warning
     # NO_TORQUE_ALERT_2 can be caused by bump or steering nudge from driver
-    ret.steerFaultTemporary = steer_status not in ("NORMAL", "LOW_SPEED_LOCKOUT", "NO_TORQUE_ALERT_2")
+    if CP.openpilotLongitudinalControl:
+      ret.steerFaultTemporary = steer_status not in ("NORMAL", "LOW_SPEED_LOCKOUT", "NO_TORQUE_ALERT_2")
+    else:
+      ret.steerFaultTemporary = steer_status not in ("NORMAL","NO_TORQUE_ALERT_2")
 
     if self.CP.carFingerprint in HONDA_BOSCH_RADARLESS:
       ret.accFaulted = bool(cp.vl["CRUISE_FAULT_STATUS"]["CRUISE_FAULT"])
