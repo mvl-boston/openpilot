2a21f40920911c5361691a2a97564962251f153c
mvl-boston <manuel@alumni.virginia.edu>
2025-02-22 19:51:04 -0500

    MDX_3G

--- opendbc_repo/opendbc/car/honda/values.py
+++ opendbc_repo/opendbc/car/honda/values.py
@@ -46,6 +46,14 @@ class CarControllerParams:
     self.STEER_LOOKUP_BP = [v * -1 for v in CP.lateralParams.torqueBP][1:][::-1] + list(CP.lateralParams.torqueBP)
     self.STEER_LOOKUP_V = [v * -1 for v in CP.lateralParams.torqueV][1:][::-1] + list(CP.lateralParams.torqueV)
 
+class LKAS_LIMITS:
+  STEER_MAX = 240
+  STEER_THRESHOLD = 15
+  STEER_DELTA_UP = 5
+  STEER_DELTA_DOWN = 9
+  STEER_DRIVER_ALLOWANCE = 25
+  STEER_DRIVER_MULTIPLIER = 18
+  STEER_DRIVER_FACTOR = 1
 
 class HondaSafetyFlags(IntFlag):
   FLAG_HONDA_ALT_BRAKE = 1
@@ -234,6 +242,18 @@ class CAR(Platforms):
     radar_dbc_dict('honda_fit_ex_2018_can_generated'),
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES,
   )
+  ACURA_MDX_3G = HondaNidecPlatformConfig(
+    [HondaCarDocs("Acura MDX 2018-2020")],
+    CarSpecs(mass=4350 * CV.LB_TO_KG, wheelbase=2.82, centerToFrontRatio=0.428, steerRatio=15.66, tireStiffnessFactor=0.444),  # acura spec, except copied stiffness from Pilot
+    radar_dbc_dict('acura_MDX_3G','acura_ilx_2016_can_generated'),
+    flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES
+  )  
+  ACURA_MDX_3G_HYBRID = HondaNidecPlatformConfig(
+    [HondaCarDocs("Acura MDX Hybrid 2018-2020")],
+    CarSpecs(mass=4486 * CV.LB_TO_KG, wheelbase=2.82, centerToFrontRatio=0.428, steerRatio=15.76, tireStiffnessFactor=0.444),  # acura spec, except copied stiffness from Pilot
+    radar_dbc_dict('acura_MDX_3G','acura_ilx_2016_can_generated'),
+    flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES
+  )  
   HONDA_ODYSSEY = HondaNidecPlatformConfig(
     [HondaCarDocs("Honda Odyssey 2018-20")],
     CarSpecs(mass=1900, wheelbase=3.0, steerRatio=14.35, centerToFrontRatio=0.41, tireStiffnessFactor=0.82),
@@ -333,12 +353,17 @@ STEER_THRESHOLD = {
   # default is 1200, overrides go here
   CAR.ACURA_RDX: 400,
   CAR.HONDA_CRV_EU: 400,
+  CAR.ACURA_MDX_3G: 25,
+  CAR.ACURA_MDX_3G_HYBRID: 25,
 }
 
 HONDA_NIDEC_ALT_PCM_ACCEL = CAR.with_flags(HondaFlags.NIDEC_ALT_PCM_ACCEL)
 HONDA_NIDEC_ALT_SCM_MESSAGES = CAR.with_flags(HondaFlags.NIDEC_ALT_SCM_MESSAGES)
 HONDA_BOSCH = CAR.with_flags(HondaFlags.BOSCH)
 HONDA_BOSCH_RADARLESS = CAR.with_flags(HondaFlags.BOSCH_RADARLESS)
-
+SERIAL_STEERING =  {CAR.ACURA_MDX_3G, CAR.ACURA_MDX_3G_HYBRID}
 
 DBC = CAR.create_dbc_map()
+
+# diag message that in some Nidec cars only appear with 1s freq if VIN query is performed
+DIAG_MSGS = {1600: 5, 1601: 8}
