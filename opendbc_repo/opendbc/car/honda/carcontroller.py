6d27bfc69104e346cd68b91f28a6e335c7a230a8
mvl-boston <manuel@alumni.virginia.edu>
2025-02-22 22:48:45 -0500

    MDX_3G

--- opendbc_repo/opendbc/car/honda/carcontroller.py
+++ opendbc_repo/opendbc/car/honda/carcontroller.py
@@ -119,28 +119,28 @@ class CarController(CarControllerBase):
 
   def apply_driver_steer_torque_limits(apply_torque, apply_torque_last, driver_torque, LIMITS, ss=False):
   
-  # limits due to driver torque
-  driver_max_torque = LIMITS.STEER_MAX + (LIMITS.STEER_DRIVER_ALLOWANCE + driver_torque * LIMITS.STEER_DRIVER_FACTOR) * LIMITS.STEER_DRIVER_MULTIPLIER
-  driver_min_torque = -LIMITS.STEER_MAX + (-LIMITS.STEER_DRIVER_ALLOWANCE + driver_torque * LIMITS.STEER_DRIVER_FACTOR) * LIMITS.STEER_DRIVER_MULTIPLIER
-  max_steer_allowed = max(min(LIMITS.STEER_MAX, driver_max_torque), 0)
-  min_steer_allowed = min(max(-LIMITS.STEER_MAX, driver_min_torque), 0)
+    # limits due to driver torque
+    driver_max_torque = LIMITS.STEER_MAX + (LIMITS.STEER_DRIVER_ALLOWANCE + driver_torque * LIMITS.STEER_DRIVER_FACTOR) * LIMITS.STEER_DRIVER_MULTIPLIER
+    driver_min_torque = -LIMITS.STEER_MAX + (-LIMITS.STEER_DRIVER_ALLOWANCE + driver_torque * LIMITS.STEER_DRIVER_FACTOR) * LIMITS.STEER_DRIVER_MULTIPLIER
+    max_steer_allowed = max(min(LIMITS.STEER_MAX, driver_max_torque), 0)
+    min_steer_allowed = min(max(-LIMITS.STEER_MAX, driver_min_torque), 0)
 
-  if ss:
+    if ss:
       min_torque = min(abs(max_steer_allowed), abs(min_steer_allowed))
       max_steer_allowed = min_torque
       min_steer_allowed = -min_torque
 
-  apply_torque = clip(apply_torque, min_steer_allowed, max_steer_allowed)
+    apply_torque = clip(apply_torque, min_steer_allowed, max_steer_allowed)
 
-  # slow rate if steer torque increases in magnitude
-  if apply_torque_last > 0:
-    apply_torque = clip(apply_torque, max(apply_torque_last - LIMITS.STEER_DELTA_DOWN, -LIMITS.STEER_DELTA_UP),
+    # slow rate if steer torque increases in magnitude
+    if apply_torque_last > 0:
+      apply_torque = clip(apply_torque, max(apply_torque_last - LIMITS.STEER_DELTA_DOWN, -LIMITS.STEER_DELTA_UP),
                         apply_torque_last + LIMITS.STEER_DELTA_UP)
-  else:
-    apply_torque = clip(apply_torque, apply_torque_last - LIMITS.STEER_DELTA_UP,
+    else:
+      apply_torque = clip(apply_torque, apply_torque_last - LIMITS.STEER_DELTA_UP,
                         min(apply_torque_last + LIMITS.STEER_DELTA_DOWN, LIMITS.STEER_DELTA_UP))
 
-  return int(round(float(apply_torque)))
+    return int(round(float(apply_torque)))
 
   
   def update(self, CC, CS, now_nanos):
