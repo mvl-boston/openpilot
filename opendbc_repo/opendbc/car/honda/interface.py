b8b2058195e0df58a29cf1b4cf89c6b9c86adaf7
mvl-boston <manuel@alumni.virginia.edu>
2025-06-20 21:30:57 -0400

    updated steer max values

--- opendbc_repo/opendbc/car/honda/interface.py
+++ opendbc_repo/opendbc/car/honda/interface.py
@@ -185,7 +185,7 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.ACURA_RDX_3G_MMR:
       if not (ret.openpilotLongitudinalControl):
         ret.minSteerSpeed = 70.0 * CV.KPH_TO_MS # min is 70kph to activate but 60kph to deactivate.  Used 70kph to clarify for warning message
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3800], [0, 3800]]
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3810], [0, 3810]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.06]]
       CarControllerParams.BOSCH_ACCEL_MAX = 1.78
       CarControllerParams.BOSCH_GAS_LOOKUP_BP = [-0.2, 1.78]
@@ -201,9 +201,7 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.HONDA_ODYSSEY_5G_MMR:
       if not ret.openpilotLongitudinalControl:
         ret.minSteerSpeed = 70.0 * CV.KPH_TO_MS # min is 70kph to activate but 60kph to deactivate.  Used 70kph to clarify for warning message
-        ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3840], [0, 3840]]  # clipped by radar
-      else:
-        ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3810], [0, 3810]]  # copied RDX, TODO: see if higher works
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.06]]
 
     elif candidate in (CAR.HONDA_PILOT, CAR.HONDA_PILOT_4G):
