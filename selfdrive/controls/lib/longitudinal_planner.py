b60cdc6d3e5e28116b41a4f0b1eb614dc83b1e44
MVL <manuel@alumni.virginia.edu>
2025-10-04 21:07:42 -0400

    time diff

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -130,7 +130,7 @@ class LongitudinalPlanner:
     # drop speed to stay within maxLateralAccel
     modelAccels = sm['modelV2'].acceleration
     max_speed = np.clip(v_ego *  np.sqrt(self.CP.maxLateralAccel / np.clip(np.abs(modelAccels.y), 0.1, None))), 4, None)
-    max_accel = np.clip((max_speed - v_ego) / np.clip(modelAccels.t, 0.1, None),ACCEL_MIN,None)
+    max_accel = np.clip((max_speed - v_ego) / np.clip(modelAccels.t - modelAccels.t[0], 0.1, None),ACCEL_MIN,None)
     if not sm['carState'].steeringPressed:
       accel_clip = np.clip(accel_clip, ACCEL_MIN, min(max_accel))
     
