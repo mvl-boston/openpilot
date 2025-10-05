84ae85094551b2e1ffe805da9165526c2773c6e5
MVL <manuel@alumni.virginia.edu>
2025-10-04 21:45:25 -0400

    more cleanup

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -34,21 +34,16 @@ def get_coast_accel(pitch):
   return np.sin(pitch) * -5.65 - 0.3  # fitted from data using xx/projects/allow_throttle/compute_coast_accel.py
 
 
-def limit_accel_in_turns(v_ego, angle_steers, a_target, CP, latAccelFactorFiltered):
+def limit_accel_in_turns(v_ego, angle_steers, a_target, CP):
   """
   This function returns a limited long acceleration allowed, depending on the existing lateral acceleration
   this should avoid accelerating when losing the target in turns
   """
   # FIXME: This function to calculate lateral accel is incorrect and should use the VehicleModel
   # The lookup table for turns should also be updated if we do this
-  min_a_target_squared = min([n ** 2 for n in a_target])
-  a_limit_car = math.sqrt(latAccelFactorFiltered ** 2 + min_a_target_squared)
-  a_total_max = min (np.interp(v_ego, _A_TOTAL_MAX_BP, _A_TOTAL_MAX_V), a_limit_car)
+  a_total_max = np.interp(v_ego, _A_TOTAL_MAX_BP, _A_TOTAL_MAX_V)
   a_y = v_ego ** 2 * angle_steers * CV.DEG_TO_RAD / (CP.steerRatio * CP.wheelbase)
   a_x_allowed = math.sqrt(max(a_total_max ** 2 - a_y ** 2, 0.))
-
-  target_speed = max(4,v_ego * latAccelFactorFiltered / max(abs(a_y),0.1))
-  a_x_allowed = min(max(-1.0, target_speed - v_ego), a_x_allowed)
 
   return [a_target[0], min(a_target[1], a_x_allowed)]
 
@@ -121,9 +116,7 @@ class LongitudinalPlanner:
     if mode == 'acc':
       accel_clip = [ACCEL_MIN, get_max_accel(v_ego)]
       steer_angle_without_offset = sm['carState'].steeringAngleDeg - sm['liveParameters'].angleOffsetDeg
-      accel_clip = limit_accel_in_turns(v_ego, steer_angle_without_offset, accel_clip, self.CP,
-#                                        sm['liveTorqueParameters'].latAccelFactorFiltered)
-                                        self.CP.maxLateralAccel)
+      accel_clip = limit_accel_in_turns(v_ego, steer_angle_without_offset, accel_clip, self.CP)
     else:
       accel_clip = [ACCEL_MIN, ACCEL_MAX]
 
@@ -133,7 +126,7 @@ class LongitudinalPlanner:
       self.a_desired = np.clip(sm['carState'].aEgo, accel_clip[0], accel_clip[1])
 
     # drop speed to stay within maxLateralAccel
-    if not sm['carState'].steeringPressed and len(sm['modelV2'].acceleration):
+    if not sm['carState'].steeringPressed and len(sm['modelV2'].acceleration.y):
       modelAccels = sm['modelV2'].acceleration
       modelSpeeds = sm['modelV2'].velocity
       max_speed = np.clip(modelSpeeds.x *  np.sqrt(self.CP.maxLateralAccel / np.clip(np.abs(modelAccels.y), 0.1, None)), 4.0, None)
