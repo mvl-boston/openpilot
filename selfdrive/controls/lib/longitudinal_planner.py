7c9f1ca0d10ef3e74e3c89a6f17b98e58f331eb2
MVL <manuel@alumni.virginia.edu>
2025-10-04 21:06:07 -0400

    Try new speed cutoff logic

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -132,6 +132,13 @@ class LongitudinalPlanner:
       # Clip aEgo to cruise limits to prevent large accelerations when becoming active
       self.a_desired = np.clip(sm['carState'].aEgo, accel_clip[0], accel_clip[1])
 
+    # drop speed to stay within maxLateralAccel
+    modelAccels = sm['modelV2'].acceleration
+    max_speed = np.clip(v_ego *  np.sqrt(self.CP.maxLateralAccel / np.clip(np.abs(modelAccels.y), 0.1, None))), 4, None)
+    max_accel = np.clip((max_speed - v_ego) / np.clip(modelAccels.t, 0.1, None),ACCEL_MIN,None)
+    if not sm['carState'].steeringPressed:
+      accel_clip = np.clip(accel_clip, ACCEL_MIN, min(max_accel))
+
     # Prevent divergence, smooth in current v_ego
     self.v_desired_filter.x = max(0.0, self.v_desired_filter.update(v_ego))
     x, v, a, j, throttle_prob = self.parse_model(sm['modelV2'])
