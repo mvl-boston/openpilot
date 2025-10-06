17211be3ce1813cd46e573fc2dedd0f1ac7b4b0f
MVL <manuel@alumni.virginia.edu>
2025-10-06 14:38:23 -0400

    try 90%

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -177,7 +177,7 @@ class LongitudinalPlanner:
       modelSpeeds = sm['modelV2'].velocity
       modelTimes = [n - modelAccels.t[0] for n in modelAccels.t]
       currentTorque = max(abs(sm['carControl'].actuators.torque),1e-3)
-      max_speed = np.clip(modelSpeeds.x *  np.sqrt(modelAccels.y[0] / currentTorque / np.clip(np.abs(modelAccels.y), 1e-3, None)), 4.0, None)
+      max_speed = np.clip(modelSpeeds.x *  np.sqrt(0.9 * modelAccels.y[0] / currentTorque / np.clip(np.abs(modelAccels.y), 1e-3, None)), 4.0, None)
       max_accel = np.clip((max_speed - v_ego) / np.clip(modelTimes, 0.1, None),ACCEL_MIN,None)
       output_a_target = min(output_a_target, min(max_accel))
       carlog.error({"output_a_target_after": output_a_target})
