a29c3c27ae2ffc8ec081fc87e5e4bd292b7a05a8
MVL <manuel@alumni.virginia.edu>
2026-04-12 11:28:39 -0400

    scroll buymeacoffee

--- selfdrive/ui/mici/layouts/home.py
+++ selfdrive/ui/mici/layouts/home.py
@@ -108,7 +108,7 @@ class MiciHomeLayout(Widget):
     self._large_version_label = UnifiedLabel("", font_size=64, text_color=rl.GRAY, font_weight=FontWeight.ROMAN, max_width=480, wrap_text=False)
     self._date_label = UnifiedLabel("", font_size=36, text_color=rl.GRAY, font_weight=FontWeight.ROMAN, max_width=480, wrap_text=False)
     self._branch_label = UnifiedLabel("", font_size=36, text_color=rl.GRAY, font_weight=FontWeight.ROMAN, scroll=True)
-    self._version_commit_label = UnifiedLabel("", font_size=36, text_color=rl.GRAY, font_weight=FontWeight.ROMAN, max_width=480, wrap_text=True)
+    self._version_commit_label = UnifiedLabel("", font_size=36, text_color=rl.GRAY, font_weight=FontWeight.ROMAN, max_width=480, wrap_text=False, scroll=True)
 
   def show_event(self):
     super().show_event()
@@ -153,7 +153,7 @@ class MiciHomeLayout(Widget):
   def _get_version_text(self) -> tuple[str, str, str, str] | None:
     version = ui_state.params.get("Version")
     branch = ui_state.params.get("GitBranch") + " " + ui_state.params.get("GitCommit")[:7]
-    commit = "buymeacoffee.com/mvlboston"
+    commit = "https://buymeacoffee.com/mvlboston"
 
     if not all((version, branch, commit)):
       return None
