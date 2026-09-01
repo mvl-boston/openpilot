a981d75f9b5459149f4eb8bf542e3c40ecd1fbe7
MVL <manuel@alumni.virginia.edu>
2026-08-31 23:34:29 -0400

    Add HondaGasAlphaParams to params_keys.h

--- openpilot/common/params_keys.h
+++ openpilot/common/params_keys.h
@@ -55,6 +55,7 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"GsmRoaming", {PERSISTENT | BACKUP, BOOL}},
     {"HardwareSerial", {PERSISTENT, STRING}},
     {"HasAcceptedTerms", {PERSISTENT, STRING, "0"}},
+    {"HondaGasAlphaParams", {PERSISTENT, FLOAT}},
     {"HondaGasFactorParams", {PERSISTENT, FLOAT}},
     {"HondaWindFactorParams", {PERSISTENT, FLOAT}},
     {"InstallDate", {PERSISTENT, TIME}},
